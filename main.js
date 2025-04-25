1243874c7d1bc8a7fff4b178e70de5f32e91643f
Giovana Pereira Sizinande <144834825+giovanapereir@users.noreply.github.com>
2025-04-25 11:52:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const botoes =document.querySelectorAll(".botao")
-const textos = document.querySelectorAll(".aba-contedo")
+const botoes =document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-contedo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
+let tempoAtual = new Date ();
 
+const tempo [0].textcontent = tempoObjetivo1-tempoAtual
+ 
 for(let i=0; i < botoes.length;i++){
     botoes[i].onclick = function(){
     
@@ -11,4 +16,23 @@ for(let i=0; i < botoes.length;i++){
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
         }
+    }
+
+
+
+    function calculaTempo(tempoObjetivo){
+        let tempoAtual= new Date ();
+        let tempofinal= tempoObjetivo-tempoAtual;
+
+        let segundo= Math.floor(tempofinal/1000)
+        let minuto= Math.floor(segundo/60)
+        let horas= Math.floor(minuto/60)
+        let dias= Math.floor(horas/24)
+
+        segundos %=60;
+        minutos %=60;
+        hora %=24;
+
+        return dias + "dias" + horas + "horas" + minutos+ "minutos"+ segundos +"segundos;"
+
     }
